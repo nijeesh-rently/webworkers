c01d3618407ad8a1e56d691f45bc665b47fa0771
Nijeesh <nijeesh.k@rently.com>
2022-07-18 22:35:14 +0530

    without worker

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,10 @@
 const file_picker = document.getElementById("file_picker")
 const canvas = document.getElementById("canvas")
 const convertButton  = document.getElementById('convert')
-const convertButtonWorker  = document.getElementById('convertButtonWorker')
 const ctx = canvas.getContext('2d');
 const blinker = document.getElementById('blinker')
 makeItBlink(blinker)
 
-const worker = new Worker('./js/worker.js');
 
 const openFile = function(event) {
     const url = URL.createObjectURL(event.target.files[0]);
@@ -22,17 +20,4 @@ convertButton.addEventListener('click', function(){
     let imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
     imgData = convertToGrayScale(imgData)
     ctx.putImageData(imgData, 0, 0);
-})
-
-
-convertButtonWorker.addEventListener('click', function(){
-    let imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
-    
-    worker.postMessage(imgData)
-
-    worker.onmessage = function(e) {
-        imgData = e.data;
-        ctx.putImageData(imgData, 0, 0);
-    }      
-
 })
--- src/js/worker.js
+++
@@ -1,7 +0,0 @@
-self.importScripts('./utils.js')
-
-onmessage = function(e) {
-    console.log(e.data);
-    const data = convertToGrayScale(e.data)
-    postMessage(data);
-}
